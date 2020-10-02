f4bdbf6c401927ca37d3292cd93cb0d730f16319
Andres Navarro <canavarro82@gmail.com>
2020-10-02 03:18:45 -0300

    Hardcoded the char buffer address size and data width

--- char_buffer.v
+++ char_buffer.v
@@ -3,14 +3,12 @@
  * Only 2000 positions used (25 lines of 80 characters)
  */
 module char_buffer (din, addr, write_en, clk, dout);
-   parameter addr_width = 11;
-   parameter data_width = 8;
-   input [addr_width-1:0] addr;
-   input [data_width-1:0] din;
+   input [10:0] addr;
+   input [7:0] din;
    input 		  write_en, clk;
-   output [data_width-1:0] dout;
-   reg [data_width-1:0]    dout; // Register for output.
-   reg [data_width-1:0]    mem [(1<<addr_width)-1:0];
+   output [7:0] dout;
+   reg [7:0]    dout; // Register for output.
+   reg [7:0]    mem [2047:0];
 
    initial
      begin
