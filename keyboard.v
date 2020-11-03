4a8796d9ee705eaf9698c40598a7ad054bdffe8b
Andres Navarro <canavarro82@gmail.com>
2020-11-03 01:36:51 -0300

    Moved all possible keycode logic to the keymap memory

--- keyboard.v
+++ keyboard.v
@@ -24,9 +24,15 @@ module keyboard
    localparam state_short_key_down = 8'b00000100;
    localparam state_long_key_up    = 8'b00001000;
    localparam state_short_key_up   = 8'b00010000;
-   localparam state_keymap_read    = 8'b00100000;
-   localparam state_esc_char       = 8'b01000000;
+   localparam state_keymap_down    = 8'b00100000;
+   localparam state_keymap_up      = 8'b01000000;
+   localparam state_esc_char       = 8'b10000000;
    localparam esc = 8'h1b;
+
+   localparam keycode_regular = 2'b0x;
+   localparam keycode_modifier = 2'b10;
+   localparam keycode_escaped = 2'b11;
+
 
    reg [7:0] state;
 
@@ -38,30 +44,25 @@ module keyboard
    reg ps2_break_keycode;
    // we are processing a long keycode (two bytes)
    reg ps2_long_keycode;
-   // shift key status
-   reg lshift_pressed;
-   reg rshift_pressed;
-   wire shift_pressed = lshift_pressed || rshift_pressed;
-   // control key status
-   reg lcontrol_pressed;
-   reg rcontrol_pressed;
-   wire control_pressed = lcontrol_pressed || rcontrol_pressed;
-   // alt/meta key status (vt52 doesn't have meta, but I want to use
-   // emacs & can't stand that Esc- business...)
-   reg lmeta_pressed;
-   reg rmeta_pressed;
-   wire meta_pressed = lmeta_pressed || rmeta_pressed;
+   // shift, control & meta key status, bit order:
+   // lshift, lcontrol, lmeta, rmeta, rcontrol, rshift
+   // alt/meta key status, vt52 doesn't have meta, but I want to use
+   // emacs & can't stand that Esc- business, so alt sends esc+keypress
+   reg [5:0] modifier_pressed;
+   wire shift_pressed = modifier_pressed[5] || modifier_pressed[0];
+   wire control_pressed = modifier_pressed[4] || modifier_pressed[1];
+   wire meta_pressed = modifier_pressed[3] || modifier_pressed[2];
    // caps lock
    reg caps_lock_active;
    // keymap
-   wire [9:0] keymap_address;
+   wire [10:0] keymap_address;
    wire [7:0] keymap_data;
    // special char to send after ESC
    reg [7:0] special_data;
 
-   // ps2_byte is the actual keycode, we use caps lock & shift to
-   // determine the plane we need
-   assign keymap_address = { caps_lock_active, shift_pressed, ps2_byte };
+   // ps2_byte is the actual keycode, we use long/short keycode, caps lock &
+   // shift to determine the plane we need
+   assign keymap_address = { ps2_long_keycode, caps_lock_active, shift_pressed, ps2_byte };
 
    keymap_rom keymap_rom(.clk(clk),
                          .addr(keymap_address),
@@ -86,12 +87,7 @@ module keyboard
          ps2_break_keycode <= 0;
          ps2_long_keycode <= 0;
 
-         lshift_pressed <= 0;
-         rshift_pressed <= 0;
-         lcontrol_pressed <= 0;
-         rcontrol_pressed <= 0;
-         lmeta_pressed <= 0;
-         rmeta_pressed <= 0;
+         modifier_pressed = 6'h00;
          caps_lock_active <= 0;
 
          special_data <= 0;
@@ -142,166 +138,77 @@ module keyboard
              end
           end
           state_long_key_up: begin
-             // we only care about the shift, control and meta states
-             state <= state_idle;
              ps2_break_keycode <= 0;
              ps2_long_keycode <= 0;
-             if (ps2_byte == 8'h14) begin
-                rcontrol_pressed <= 0;
-             end
-             else if (ps2_byte == 8'h11) begin
-                rmeta_pressed <= 0;
-             end
+             state <= state_keymap_up;
           end
           state_short_key_up: begin
-             // we only care about the shift, control and meta states
-             state <= state_idle;
              ps2_break_keycode <= 0;
-             ps2_long_keycode <= 0;
-             if (ps2_byte == 8'h12) begin
-                lshift_pressed <= 0;
-             end
-             else if (ps2_byte == 8'h59) begin
-                rshift_pressed <= 0;
-             end
-             else if (ps2_byte == 8'h14) begin
-                lcontrol_pressed <= 0;
-             end
-             else if (ps2_byte == 8'h11) begin
-                lmeta_pressed <= 0;
-             end
+             state <= state_keymap_up;
           end
           state_long_key_down: begin
-             // we care about the shift, control and meta states
-             // and also arrows, del & insert (line feed)
-             state <= state_idle;
-             ps2_break_keycode <= 0;
              ps2_long_keycode <= 0;
-             if (ps2_byte == 8'h14) begin
-                rcontrol_pressed <= 1;
-             end
-             else if (ps2_byte == 8'h11) begin
-                rmeta_pressed <= 1;
-             end
-             else if (ps2_byte == 8'h71) begin
-                // DEL key, send either DEL or US (if control pressed)
-                data <= {
-                         1'b0,
-                         control_pressed? 2'b00 : 2'b11,
-                         5'b11111
-                         };
-                valid <= 1;
-             end
-             else if (ps2_byte == 8'h70) begin
-                // INS key, send line feed char
-                data <= 8'h0a;
-                valid <= 1;
-             end
-             else if (ps2_byte == 8'h75) begin
-                // up arrow, send either Esc-A or Esc-C^A (if control pressed)
-                data <= esc;
-                valid <= 1;
-                state <= state_esc_char;
-                special_data <= {
-                                 1'b0,
-                                 control_pressed? 2'b00 : 2'b11,
-                                 5'b00001
-                                };
-             end
-             else if (ps2_byte == 8'h72) begin
-                // down arrow, send either Esc-B or Esc-C^B (if control pressed)
-                data <= esc;
-                valid <= 1;
-                state <= state_esc_char;
-                special_data <= {
-                                 1'b0,
-                                 control_pressed? 2'b00 : 2'b11,
-                                 5'b00010
-                                 };
-             end
-             else if (ps2_byte == 8'h74) begin
-                // right arrow, send either Esc-C or Esc-C^C (if control pressed)
-                data <= esc;
-                valid <= 1;
-                state <= state_esc_char;
-                special_data <= {
-                                 1'b0,
-                                 control_pressed? 2'b00 : 2'b11,
-                                 5'b00011
-                                 };
-             end
-             else if (ps2_byte == 8'h6b) begin
-                // left arrow, send either Esc-d Esc-C^D (if control pressed)
-                data <= esc;
-                valid <= 1;
-                state <= state_esc_char;
-                special_data <= {
-                                 1'b0,
-                                 control_pressed? 2'b00 : 2'b11,
-                                 5'b00100
-                                 };
-             end
+             state <= state_keymap_down;
           end
           state_short_key_down: begin
-             // default value, may be overridden
+             state <= state_keymap_down;
+          end
+          state_keymap_up: begin
              state <= state_idle;
-             ps2_break_keycode <= 0;
-             ps2_long_keycode <= 0;
-             if (ps2_byte == 8'h12) begin
-                lshift_pressed <= 1;
+             if (keymap_data[7:6] == keycode_modifier) begin
+                // the released modifier is in keymap_data[5:0]
+                modifier_pressed <= modifier_pressed & ~keymap_data[5:0];
              end
-             else if (ps2_byte == 8'h59) begin
-                rshift_pressed <= 1;
-             end
-             if (ps2_byte == 8'h14) begin
-                lcontrol_pressed <= 1;
-             end
-             else if (ps2_byte == 8'h11) begin
-                lmeta_pressed <= 1;
-             end
-             else if (ps2_byte == 8'h58) begin
-                caps_lock_active <= ~caps_lock_active;
-             end
-             else if (ps2_byte == 8'h05) begin
-                // F1, map to left blank key: Esc-P
-                state <= state_esc_char;
-                special_data <= "P";
-                data <= esc;
-                valid <= 1;
-             end
-             else if (ps2_byte == 8'h06) begin
-                // F1, map to center blank key: Esc-Q
-                state <= state_esc_char;
-                special_data <= "Q";
-                data <= esc;
-                valid <= 1;
-             end
-             else if (ps2_byte == 8'h04) begin
-                // F1, map to right blank key: Esc-R
-                state <= state_esc_char;
-                special_data <= "R";
-                data <= esc;
-                valid <= 1;
+          end
+          state_keymap_down: begin
+             if (keymap_data == 0) begin
+                state <= state_idle;
              end
              else begin
-                // regular keys, read from keymap ROM
-                state <= state_keymap_read;
-             end
-          end
-          state_keymap_read: begin
-             state <= state_idle;
-             if (keymap_data != 0) begin
                 // apply modifier keys
-                // meta turns on the 8th bit
+                // meta sends an ESC prefix
                 // control turns off 7th & 6th bits
-                data <= {
-                         meta_pressed,
-                         control_pressed? 2'b00 : keymap_data[6:5],
-                         keymap_data[4:0]
-                        };
-                valid <= 1;
-             end
-          end
+                casex (keymap_data[7:6])
+                  keycode_regular: begin
+                     if (meta_pressed) begin
+                        data <= esc;
+                        valid <= 1;
+                        state <= state_esc_char;
+                        special_data <= {
+                                         1'b0,
+                                         control_pressed? 2'b00 : keymap_data[6:5],
+                                         keymap_data[4:0]
+                                         };
+                     end
+                     else begin
+                        data <= {
+                                 1'b0,
+                                 control_pressed? 2'b00 : keymap_data[6:5],
+                                 keymap_data[4:0]
+                                 };
+                        valid <= 1;
+                        state <= state_idle;
+                     end
+                  end
+                  keycode_escaped: begin
+                     data <= esc;
+                     valid <= 1;
+                     state <= state_esc_char;
+                     special_data <= {
+                                      1'b0,
+                                      control_pressed? 2'b00 : keymap_data[6:5],
+                                      keymap_data[4:0]
+                                      };
+                  end
+                  keycode_modifier: begin
+                     // the pressed modifier is in keymap_data[5:0], or 0 for caps lock
+                     state <= state_idle;
+                     modifier_pressed <= modifier_pressed | keymap_data[5:0];
+                     caps_lock_active <= caps_lock_active ^ ~|keymap_data[5:0];
+                  end
+                endcase
+             end // else: !if(keymap_data == 0)
+          end // case: state_keymap_down
           state_esc_char: begin
              // only send special char after ESC was successfully sent
              if (valid == 0) begin
--- keymap_rom.v
+++ keymap_rom.v
@@ -1,7 +1,8 @@
 /**
- * Keymap ROM (1KB, maps keycodes to ASCII chars)
+ * Keymap ROM (2KB, maps keycodes to ASCII chars)
  * This could be a RAM to allow keymap modifications, but not for now
- * There's 4 planes for each keycode (controlled by the highest two bits)
+ * There's 8 planes for each keycode (controlled by the highest three bits)
+ * MSB is for long keycode vs short keycode, the the next two bits:
  * 00: no shift or caps lock
  * 01: just shift
  * 10: just caps lock
@@ -9,12 +10,12 @@
  */
 module keymap_rom
   (input clk,
-   input [9:0] addr,
+   input [10:0] addr,
    output [7:0] dout
    );
 
    reg [7:0]    dout;
-   reg [7:0]    mem [1023:0];
+   reg [7:0]    mem [2047:0];
 
    integer i;
 
@@ -22,7 +23,7 @@ module keymap_rom
       // the hex file is sparse, prefill with zeros
       // XXX yosys doesn't like this, it overrides the readmemh
       // so for now just assume that all other positions have zeros...
-      // for (i = 0; i < 1023; i = i + 1) mem[i] = "b";
+      // for (i = 0; i < 2047; i = i + 1) mem[i] = "b";
       $readmemh("mem/keymap.hex", mem) ;
    end
 
